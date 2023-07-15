794daf3c8907293d09d272b2b7fd8a78868fda94
Denner Portuguez Villalobos <dennerportuguez@gmail.com>
2023-07-14 19:15:24 -0600

    Fix: Bug in router redirect

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter, redirect } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Users from "./pages/Users";
 
 export const router = createBrowserRouter([
-  { path: '/', loader: () => redirect('/') },
+  { path: '/', element: <Navigate to="/users" replace /> },
   { path: '/users', element: <Users /> },
   { path: '/users/:userId', element: <Users /> },
 ]);
